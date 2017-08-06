371cf1ac3662009392fcd67b6a7914e5f1f97eb4
Mateusz Nowak <mateusznowak@vp.pl>
2017-08-06 21:33:20 +0200

    Initial commit

---
+++ src/main/java/com/cct/carcosttracker/CarCostTrackerApplication.java
@@ -0,0 +1,12 @@
+package com.cct.carcosttracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarCostTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarCostTrackerApplication.class, args);
+	}
+}
